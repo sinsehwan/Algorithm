496e73c351c134af426d9cd08414f9e03bf21329
sinsehwan <91408891+sinsehwan@users.noreply.github.com>
2024-11-22 21:42:49 +0900

    [level 1] Title: 둘만의 암호, Time: 1.56 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -3,38 +3,23 @@ import java.io.*;
 
 class Solution {
     public String solution(String s, String skip, int index) {
-        char[] answer = new char[s.length()];
-        
-        char[] abc = new char[26 - skip.length()];
-        int abcIndex = 0;
-        for(char c = 'a'; c <= 'z'; c++) {
-            if(skip.indexOf(c) == -1){
-                abc[abcIndex] = c;
-                abcIndex += 1;
+           
+        StringBuilder answer = new StringBuilder();
+        for(char letter : s.toCharArray()){
+            char temp = letter;
+            
+            int idx = 0;
+            
+            while(idx < index){
+                temp = temp == 'z' ? 'a' : (char) (temp + 1);
+                if(!skip.contains(String.valueOf(temp))){
+                    idx += 1;
+                }
             }
+            
+            answer.append(temp);
+            
         }
-        //
-        //for (char item : abc){
-        //    System.out.print(item);
-        //}
-        //System.out.println();
-        
-        for(int i = 0; i < s.length(); i++){
-            int pIndex = findIndex(abc, 26 - skip.length(), s.charAt(i));
-            answer[i] = abc[(pIndex + index) % (26 - skip.length())];
-            //System.out.println(i + " " + answer[i] + " " + pIndex);
-        }
-        return new String(answer);
-    }
-    
-    public int findIndex(char[] arr, int arrSize, char value){
-        int result = -1;
-        for(int i = 0; i < arrSize; i++){
-            if (arr[i] == value){
-                result = i;
-                break;
-            }
-        }
-        return result;
+        return answer.toString();
     }
 }
