a980d8848bb72d3178cba03365a1f0e302660107
sinsehwan <91408891+sinsehwan@users.noreply.github.com>
2025-06-03 16:43:40 +0900

    [Silver I] Title: 회전 초밥, Time: 160 ms, Memory: 18584 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -2,54 +2,69 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) throws IOException{
+    int n, d, k, c;
+    int[] cho;
+    int[] choCount;
+
+    public static void main(String[] args) throws IOException {
         new Main().solve();
 
         br.close();
         bw.close();
     }
 
-    void solve() throws IOException{
-        int n, d, k, c;
+    void solve() throws IOException {
+        getInput();
+
+        int count = init();
+        int best = count;
+
+        for(int i = 0; i < n; i++){
+            choCount[cho[i]] += 1;
+            if(choCount[cho[i]] == 1){
+                count += 1;
+            }
+
+            choCount[cho[(n - k + i) % n]] -= 1;
+            if(choCount[cho[(n - k + i) % n]] == 0){
+                count -= 1;
+            }
+
+            best = Math.max(count, best);
+        }
+
+        bw.write(best + "");
+    }
+
+    void getInput() throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         d = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
 
-        int[] sushi = new int[n];
-        int[] type = new int[d + 1];
+        cho = new int[n];
+        choCount = new int[d + 1];
 
         for(int i = 0; i < n; i++){
-            sushi[i] = Integer.parseInt(br.readLine());
+            cho[i] = Integer.parseInt(br.readLine());
         }
-
-        int answer = 0;
-
-        for(int i = n - k; i < n; i++){
-            type[sushi[i]] += 1;
-        }
-
-        answer = Math.max(answer, countType(type));
-
-        type[c] += 1;
-        for(int i = 0; i < n; i++){
-            int j = (i + n - k) % n;
-            type[sushi[j]] -= 1;
-            type[sushi[i]] += 1;
-            answer = Math.max(answer, countType(type));
-        }
-
-        bw.write(answer + "");
     }
 
-    int countType(int[] type) {
+    int init(){
+        // c는 깍두기. 0이 될 수 없다.
+        choCount[c] += 1;
+        for(int i = n - k; i < n; i++){
+            choCount[cho[i]] += 1;
+        }
+
         int count = 0;
-        for(int i = 1; i < type.length; i++){
-            if(type[i] > 0){
+        for(int i = 1; i <= d; i++){
+            if(choCount[i] > 0){
                 count += 1;
             }
         }
